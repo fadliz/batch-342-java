09d4875cf1a3c3d298ec5bfedc37ce01ad6856aa
fadliz <asama.fadli@gmail.com>
2024-09-20 13:13:55 +0700

    [ADD] Matrix/triangle print functions

--- src/App.java
+++ src/App.java
@@ -3,15 +3,22 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         try (Scanner scn = new Scanner(System.in)) {
-            // DailyTraining  training = new DailyTraining();
+            DayFour training = new DayFour();
             
             // System.out.print("Input year : ");
             // int year = Integer.parseInt(scn.nextLine());
             // training.leapYear(year);
 
-            // System.out.print("Input number : ");
+            // System.out.print("Input Row : ");
             // int number = Integer.parseInt(scn.nextLine());
-            // training.oddEvenAddition(number);
+            System.out.print("Input Number : ");
+            int numberTwo = Integer.parseInt(scn.nextLine());
+            // training.matrixStarPrint(numberTwo);
+            System.out.print("Is the triangle reversed (true/false)? ");
+            boolean reverse = Boolean.parseBoolean(scn.nextLine());
+            training.triangleNumPrint(numberTwo, reverse);
+            System.out.println();
+            training.triangleNumPrintAsterisk(numberTwo, reverse);
 
             scn.close();
         }
