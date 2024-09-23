1f891457552e94e2f6987f2fc4f22ddff9b63a2c
fadliz <asama.fadli@gmail.com>
2024-09-23 11:25:35 +0700

    [ADD] array statistical calculation

--- src/App.java
+++ src/App.java
@@ -1,18 +1,27 @@
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         try (Scanner scn = new Scanner(System.in)) {
             DayFive training = new DayFive();
-            
+
             System.out.print("Input Number : ");
             int number = Integer.parseInt(scn.nextLine());
-            // training.patternedAsteriskOne(number);
-            training.reversedCountMatrix(number);
+            int[] arr = new int[number];
+            Arrays.setAll(arr, i -> (new Random()).nextInt(50));
+            System.out.println(training.calculateMax(arr) + " " +
+                    training.calculateMin(arr) + " " +
+                    training.calculateMean(arr) + " " +
+                    training.calculateMedian(arr) + " " +
+                    training.calculateMode(arr));
+
             System.out.println();
-            // training.patternedAsteriskTwo(number);
-            training.oddEvenCountMatrix(number);
-            
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+
             scn.close();
         }
     }
--- src/DayFive.java
+++ src/DayFive.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class DayFive {
     public void patternedAsteriskOne(int n) {
         for (int i = 0; i < n; i++) {
@@ -31,13 +33,63 @@ public class DayFive {
             for (int j = 0; j < n; j++) {
                 if (i % 2 == 0) {
                     // 1 + (i * 2) = 3
-                    System.out.print(((n * 2 * (i/2)) + (1 + (j * 2))) + "\t");
+                    System.out.print(((n * 2 * (i / 2)) + (1 + (j * 2))) + "\t");
                 } else {
                     // (i + 1) kali 2
-                    System.out.print(((n * 2 * (i/2)) + ((1 + j) * 2)) + "\t");
+                    System.out.print(((n * 2 * (i / 2)) + ((1 + j) * 2)) + "\t");
                 }
             }
             System.out.println();
         }
     }
+
+    public int calculateMean(int[] arr) {
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        return sum / arr.length;
+    }
+
+    public int calculateMedian(int[] arr) {
+        Arrays.sort(arr);
+        if (arr.length % 2 == 1) {
+            return arr[arr.length / 2];
+        }
+        return (arr[arr.length / 2 - 1] + arr[arr.length / 2]) / 2;
+    }
+
+    public int calculateMode(int[] arr) {
+        Arrays.sort(arr);
+        int count = 1, maxCount = 1, mode = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                mode = arr[i - 1];
+            }
+        }
+        return mode;
+    }
+
+    public int calculateMin(int[] arr) {
+        int min = arr[0];
+        for (int i : arr) {
+            min = i < min ? i : min;
+        }
+        return min;
+    }
+
+    public int calculateMax(int[] arr) {
+        int max = arr[0];
+        for (int i : arr) {
+            max = i > max ? i : max;
+        }
+        return max;
+    }
+
 }
