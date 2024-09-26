93bc02c7d3b4abbf305b9cde91236789abab24d3
fadliz <asama.fadli@gmail.com>
2024-09-26 11:01:12 +0700

    [ADD] Calculator class

--- src/App.java
+++ src/App.java
@@ -1,10 +1,21 @@
+
+import calculator.Calculator;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        try (Scanner scn = new Scanner(System.in)) {
-            scn.close();
-        }
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        String cont;
+        do {
+            System.out.print((char)27+"[0m");
+            calculator.calculate();
+            System.out.println("Result : " + calculator.getResult());
+            calculator.outputResultHistory();
+            System.out.print((char)27+"[0m");
+            System.out.print((char)27+"[1mContinue (y/n)?");
+            cont = scanner.nextLine();
+        } while (cont.equalsIgnoreCase("y"));
     }
 
 }
