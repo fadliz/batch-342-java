acc3ad3dd556328cb124b9f7c4642923232cf75a
fadliz <asama.fadli@gmail.com>
2024-09-26 08:29:07 +0700

    [ADD] trainings package

--- src/App.java
+++ src/App.java
@@ -3,10 +3,6 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         try (Scanner scn = new Scanner(System.in)) {
-            DaySeven training = new DaySeven();
-            long balance = 0;
-            training.moneyTransferProgram(balance);
-
             scn.close();
         }
     }
