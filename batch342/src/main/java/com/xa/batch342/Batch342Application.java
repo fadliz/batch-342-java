7dbda61c1ee68b950321f87a652f55f561f927d7
fadliz <asama.fadli@gmail.com>
2024-10-14 13:37:10 +0700

    [ADD] java springboot maven project

---
+++ batch342/src/main/java/com/xa/batch342/Batch342Application.java
@@ -0,0 +1,13 @@
+package com.xa.batch342;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batch342Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Batch342Application.class, args);
+	}
+
+}
